120099ba28bd74cc3fbbf597874e385f9220fb88
Jeevitha Royapathi <jeevitha.royapathi@xero.com>
2016-08-26 15:00:49 -0700

    checks for network and location availability

--- app/src/main/java/com/transitangel/transitangel/details/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/transitangel/transitangel/details/AlarmBroadcastReceiver.java
@@ -3,12 +3,40 @@ package com.transitangel.transitangel.details;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.location.LocationManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.transitangel.transitangel.R;
 
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
+    public static final int NOTIFICATION_LOCATION_UNAVAILABLE=100;
+    private String TAG=AlarmBroadcastReceiver.class.getSimpleName();
     @Override
     public void onReceive(Context context, Intent intent) {
-        //TODO : check for network/gps for location update
-        Log.e(AlarmBroadcastReceiver.class.getSimpleName(),"inside OnAlarm");
+        LocationManager locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+        try {
+            network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+        if(!gps_enabled && !network_enabled)
+        {
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            String title = "Location Unavailable";
+            String contentTitle = "Unable to locate your location";
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.train)
+                            .setContentTitle(title)
+                            .setContentText(contentTitle)
+                            .setTicker(contentTitle)
+                            .setAutoCancel(true);
+            notificationManager.notify(NOTIFICATION_LOCATION_UNAVAILABLE, mBuilder.build());
+        }
     }
 }
