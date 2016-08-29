156fe7eb09f658ac9c808508cff8e683fac1f33b
Vidhur <lab.nirvana@gmail.com>
2016-08-29 12:49:37 -0700

    Modified the geofence radius to 3 km as per Yogesh suggestion

--- app/src/main/java/com/transitangel/transitangel/model/Transit/TrainStopFence.java
+++ app/src/main/java/com/transitangel/transitangel/model/Transit/TrainStopFence.java
@@ -16,7 +16,7 @@ public class TrainStopFence implements Comparable {
     public String fenceId;
 
     public TrainStopFence(TrainStop trainStop) {
-        this(trainStop, 2.5f);
+        this(trainStop, 3.0f);
     }
 
     private TrainStopFence(TrainStop trainStop,float radiusKm) {
